93c02edc694dc434cad4e7da2abf3039fdfb0229
William C. Canin <william.costa.canin@gmail.com>
2024-09-04 14:02:22 -0300

    Adding new field in model

--- example1/src/users/model.rs
+++ example1/src/users/model.rs
@@ -3,14 +3,15 @@ pub struct UserFields {
   pub name: String,
   pub email: String,
   pub age: i32,
+  pub driver_license: bool,
 }
 
 impl UserFields {
   pub fn fields(&self) -> Vec<&str> {
-    vec!["name", "email", "age"]
+    vec!["name", "email", "age", "driver_license"]
   }
 
   pub fn values(&self) -> Vec<&(dyn tokio_postgres::types::ToSql + Sync)> {
-    vec![&self.name, &self.email, &self.age]
+    vec![&self.name, &self.email, &self.age, &self.driver_license]
   }
 }
